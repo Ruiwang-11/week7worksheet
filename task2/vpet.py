a481a2781db056a175674bcff9a39117e4c07927
Ruiwang-11 <jvcs5288@leeds.ac.uk>
2024-11-21 22:51:08 +0000

    added work

---
+++ task2/vpet.py
@@ -0,0 +1,26 @@
+
+class VirtualPet:
+    def __init__(self, name):
+        self.name = name 
+        self.energy = 10 
+        self.hunger = 0 
+
+    def play(self):
+
+        if self.energy < 2:
+            print("Too tired to play")
+        else:
+            self.energy -= 2 
+            self.hunger += 2 
+
+    def feed(self):
+
+        self.hunger = max(0, self.hunger - 3)
+
+    def sleep(self):
+
+        self.energy += 10
+
+    def __str__(self):
+
+        return f"{self.name} with {self.energy} energy points and {self.hunger} hunger level"
